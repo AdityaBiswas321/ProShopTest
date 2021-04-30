e24f8a2c4bc2d54821cc9d5ed67425e70ce204b3
Aditya <aditya@DESKTOP-A4S7E29.localdomain>
2021-04-29 18:16:05 -0700

    Prepare for deployment

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,22 +7,22 @@ import User from '../models/userModel.js'
 //@ access Public
 
 const authUser = AsyncHandler(async (req, res) => {
-    const { email, password } = req.body
+  const { email, password } = req.body
 
-    const user = await User.findOne({ email })
+  const user = await User.findOne({ email })
 
-    if(user && (await user.matchPassword(password))) {
-        res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin,
-            token: generateToken(user._id)
-        })
-    } else {
-        res.status(401)
-        throw new Error('Invalid email or password')
-    }
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid email or password')
+  }
 })
 
 //@ desc  REgister a new user
@@ -30,105 +30,170 @@ const authUser = AsyncHandler(async (req, res) => {
 //@ access Public
 
 const registerUser = AsyncHandler(async (req, res) => {
-    const { name, email, password } = req.body  
+  const { name, email, password } = req.body
 
-    if(!name) {
-        res.status(400)
-        throw new Error('Name is Required')
-        
-    }
+  if (!name) {
+    res.status(400)
+    throw new Error('Name is Required')
+  }
 
-    if(!email) {
-        res.status(400)
-        throw new Error('Email is Required')
-    } 
-    
-    if(!password) {
-        res.status(400)
-        throw new Error('Password is Required')
-    }
+  if (!email) {
+    res.status(400)
+    throw new Error('Email is Required')
+  }
 
-    const userExists = await User.findOne({ email })
+  if (!password) {
+    res.status(400)
+    throw new Error('Password is Required')
+  }
 
-    if(userExists) {
-        res.status(400)
-        throw new Error('User already exists')
-    }
+  const userExists = await User.findOne({ email })
 
-    const user = await User.create({
-        name,
-        email,
-        password
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
     })
-
-    if(user) {
-        res.status(201).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin,
-            token: generateToken(user._id)
-        })
-    } else {
-        res.status(400)
-        throw new Error('Invalid user data')
-    }
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 })
-
 
 //@ desc  Get user profile
 //@ route GET /api/user/profile
 //@ access Private
 
 const getUserProfile = AsyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id)
+  const user = await User.findById(req.user._id)
 
-    if(user) {
-        res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin,
-        })
-    } else {
-        res.status(404)
-        throw new Error('User not found')
-}})
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
 
 //@ desc  Update user profile
 //@ route PUT /api/user/profile
 //@ access Private
 
 const updateUserProfile = AsyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id)
-    console.log(user)
-    if(user) {
-        user.name = req.body.name || user.name
-        user.email = req.body.email || user.email
-        if(req.body.password) {
-            user.password =req.body.password
-        }
+  const user = await User.findById(req.user._id)
+  console.log(user)
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if (req.body.password) {
+      user.password = req.body.password
+    }
 
-        const updateUser = await user.save()
+    const updateUser = await user.save()
 
-        res.json({
-            _id: updateUser._id,
-            name: updateUser.name,
-            email: updateUser.email,
-            isAdmin: updateUser.isAdmin,
-            token: generateToken(updateUser._id)
-        }) 
-    } else {
-        res.status(404)
-        throw new Error('User not found')
-}})
+    res.json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+      token: generateToken(updateUser._id),
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
 
+//@ desc  Get all users
+//@ route GET /api/user
+//@ access Private/Admin
 
+const getUsers = AsyncHandler(async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+})
 
+//@ desc  Delete user
+//@ route DELETE /api/user/:id
+//@ access Private/Admin
+
+const deleteUser = AsyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+  if (user) {
+    await user.remove()
+    res.json({ message: 'User removed' })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+//@ desc  Get user by ID
+//@ route GET /api/user/:id
+//@ access Private/Admin
+
+const getUsersById = AsyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password')
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+//@ desc  Update
+//@ route PUT /api/user/:id
+//@ access Private/Admin
+
+const updateUser = AsyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.isAdmin === true ? true : false
+
+    const updateUser = await user.save()
+
+    res.json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
 
 export {
-    authUser,
-    getUserProfile,
-    registerUser,
-    updateUserProfile
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUsersById,
+  updateUser,
 }
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,27 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
-
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
 connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
 //for req.body
 app.use(express.json())
-
-app.get('/', (req, res) => {
-    res.send('API is RUNNING....')
-})
 
 app.use('/api/products', productRoutes)
 
@@ -26,20 +29,45 @@ app.use('/api/users', userRoutes)
 
 app.use('/api/orders', orderRoutes)
 
+app.use('/api/upload', uploadRoutes)
+
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is RUNNING....')
+  })
+}
+
 app.use(notFound)
 
 app.use(errorHandler)
 
 app.get('/api/products', (req, res) => {
-    res.json(products)
+  res.json(products)
 })
 
-
 app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
 })
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode port ${PORT}`.yellow.bold))
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode port ${PORT}`.yellow.bold
+  )
+)
